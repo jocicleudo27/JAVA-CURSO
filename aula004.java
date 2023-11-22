72962c63713dc8933512358cd6167d83f9a5abfd
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-22 20:58:54 -0300

    ler entrada de dados

--- aula004.java
+++ aula004.java
@@ -6,5 +6,13 @@ public class aula004{
         Scanner scan = new Scanner(System.in);
 
         int n1 = 0, n2 = 0, res = 0;
+        // ler entrada de dados.
+
+        System.out.printl("Digite um numero: ");
+        n1 = scan.nextInt();
+        System.out.printl("Digite outro numero: ");
+        n2 = scan.nextInt();
+        res = n1 + n2;
+        System.out.printf("Soma de %d com %d e igual a %d", n1, n2, res);
     }
 }
