1f58e13b3a878fb0429623b1cb6f249cee033332
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-22 15:30:47 -0300

    testando as estrururas condiciocionais
    mudei o valor da variável nota.

--- aula002java.java
+++ aula002java.java
@@ -1,11 +1,13 @@
 public class aula002java{
     public static void main(String[]args){
 
-        int nota = 65;
+        int nota = 50;
         int media = 60;
 
         if(nota >= media){
             System.out.println("Aprovado");
+        }else{
+        System.out.println("Reprovado");
         }
         System.out.println("FIm do programa");
     }
