22e939dea080bc322c5151692eb7f24ab5542777
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-22 20:45:37 -0300

    declaração de variaveis

---
+++ aula004.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class aula004{
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        int n1 = 0, n2 = 0, res = 0;
+    }
+}
