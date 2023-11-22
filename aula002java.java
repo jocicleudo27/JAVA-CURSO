d0b96bab2299dfdd9f82e8afcd518a5ebed17a4a
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-22 15:41:51 -0300

    teste recuperação

--- aula002java.java
+++ aula002java.java
@@ -1,11 +1,13 @@
 public class aula002java{
     public static void main(String[]args){
 
-        int nota = 50;
+        int nota = 40; //teste
         int media = 60;
 
         if(nota >= media){
             System.out.println("Aprovado");
+        }else if(nota >= 40){
+            System.out.println("Recuperacao");
         }else{
         System.out.println("Reprovado");
         }
