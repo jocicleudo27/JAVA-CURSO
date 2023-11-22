3703a05e23d3694b9582cf5161abda76e9a72a83
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-22 06:41:54 -0300

    diferença entre as forma de print

--- olamundo.java
+++ olamundo.java
@@ -2,5 +2,8 @@ public class olamundo {
 
     public static void main(String[] args){
         System.out.print("Ola Mundo!");
+        //pirnt não quebra linha, println quebra linha, printf formata
+        System.out.println("CFB curso de Java.");
+        System.out.printf("Canal:%s - Ano:%d", "CFB Cursos",22112023);
     }
 }
