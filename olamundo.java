57c2aed724e583d1799cc45a22234af13ac67482
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-22 06:58:22 -0300

    uso de variaveis

--- olamundo.java
+++ olamundo.java
@@ -1,9 +1,14 @@
 public class olamundo {
 
     public static void main(String[] args){
+       
+       int num = 100;
+       String nome = "Jocicleudo";
+
         System.out.print("Ola Mundo!");
         //pirnt não quebra linha, println quebra linha, printf formata
         System.out.println("CFB curso de Java.");
         System.out.printf("Canal:%s - Ano:%d", "CFB Cursos",22112023);
+        System.out.printf("Valor da variavel num:%d%nNome:%s", num, nome);
     }
 }
