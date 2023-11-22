8896a63f1175f8c6b17cad192d260670d3b3a478
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-22 19:35:05 -0300

    switch case

---
+++ aula003.java
@@ -0,0 +1,20 @@
+public class aula003{
+    public static void main(string[]args){
+        int pos = 1;
+
+        switch(pos){
+            case 1:
+                System.out.println("Primeiro Lugar");
+                break;
+            case 2:
+                System.out.println("Segundo Lugar");
+                break;
+            case 3:
+                System.out.println("Terceiro Lugar");
+                break;
+            default:
+            System.out.println("Nao subiu no podium");
+                break;
+        }
+    }
+}
