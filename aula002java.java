114e0a0c9f8196ad5abfef8cd35135e7e8c34322
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-22 15:52:38 -0300

    variáveis faltas e maxFaltas adicinadas

--- aula002java.java
+++ aula002java.java
@@ -1,10 +1,12 @@
 public class aula002java{
     public static void main(String[]args){
 
-        int nota = 40; //teste
+        int nota = 85; //teste
+        int faltas = 10;
+        int maxFaltas = 5;
         int media = 60;
 
-        if(nota >= media){
+        if(nota >= media && faltas <= maxFaltas){
             System.out.println("Aprovado");
         }else if(nota >= 40){
             System.out.println("Recuperacao");
