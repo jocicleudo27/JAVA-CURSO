1b95fbfe50168ca8f32606af5510a8083f0ae33f
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-23 16:58:19 -0300

    tcompile ok

--- aula005.java
+++ aula005.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
 public class aula005 {
-
+    public static void main(String[] args){
     //aula sobre loop
     //FOR
     for(int cont = 0; cont < 5; cont++){
         System.out.println("CFB Cursos");
     }
-    
+}
+   
+}   
