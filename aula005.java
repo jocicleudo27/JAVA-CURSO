68902c60088287169359440220b7478379c060a0
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-23 16:36:44 -0300

    23/11/23 loop for

---
+++ aula005.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class aula005 {
+
+    //aula sobre loop
+    //FOR
+    for(int cont = 0; cont < 5; cont++){
+        System.out.println("CFB Cursos");
+    }
+    
+}
+
+}
