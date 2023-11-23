4c2a16e499189e47655a102ed35531c7441ea04d
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-23 17:32:19 -0300

    loop while

--- aula005.java
+++ aula005.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
 
-public class aula005 {
-    public static void main(String[] args){
-    //aula sobre loop
-    //FOR
+    public class aula005 {
+        public static void main(String[] args){
+        //aula sobre loop
+        //FOR
+        /*
         for(int cont = 5; cont > 0; cont--){
+                System.out.println(cont +" - CFB Cursos");
+            }
+            */
+            //WILIE
+        int cont =0;
+        while (cont < 5) {
             System.out.println(cont +" - CFB Cursos");
+            cont++;
         }
         System.out.println("Fim do programa");
-    }
+    }   
+    
+}
    
-}   
+    
