ae4b591f7b7a72b336eb15f8de8495d4091ca498
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-23 17:06:24 -0300

    cont

--- aula005.java
+++ aula005.java
@@ -5,7 +5,7 @@ public class aula005 {
     //aula sobre loop
     //FOR
         for(int cont = 5; cont > 0; cont--){
-            System.out.println(cont +"CFB Cursos");
+            System.out.println(cont +" - CFB Cursos");
         }
         System.out.println("Fim do programa");
     }
