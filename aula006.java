4903e714c7a5967c6808da32b9f85394ded01e77
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-23 18:44:59 -0300

    estudo dos arrays

--- aula006.java
+++ aula006.java
@@ -3,6 +3,7 @@ public class aula006{
     public static void main(String[] args){
 
         int[] num = new int[5];
+        int[] neme = {10,20,30,40,50};
 
         num[0] = 10;
         num[1] = 5;
@@ -10,6 +11,7 @@ public class aula006{
         num[3] = 0;
         num[4] =8;
 
-        System.out.printf("%d", num[3]);
+        System.out.printf("%d\n", num[2]);
+        System.out.printf("%d", neme[1]);
     }
 }
