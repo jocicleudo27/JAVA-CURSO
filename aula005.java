a67147752a6f1ac1e1697cdf6749647ccf374d37
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-23 18:09:23 -0300

    do while feito com sucesso

--- aula005.java
+++ aula005.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
     public class aula005 {
         public static void main(String[] args){
         
-        Scanner  scan = new Scanner(System.in);
+        //Scanner  scan = new Scanner(System.in);
         
         //aula sobre loop
         //FOR
@@ -13,12 +13,20 @@ import java.util.Scanner;
             }
             */
             //WILIE
-        int max = scan.nextInt();
+        //int max = scan.nextInt();
+        /*
         int cont =0;
         while (cont < max) {
             System.out.println(cont +" - CFB Cursos");
             cont++;
-        } 
+        }
+        */
+        //Do While
+        int cont = 0;
+        do {
+            System.out.println(cont +" - CFB Cursos");
+            cont++;
+        } while (cont <= 0);
         System.out.println("Fim do programa");
     }   
     
