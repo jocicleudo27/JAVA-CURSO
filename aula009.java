72b23773c7d86f531d16538b87f320be6733640f
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-24 07:50:33 -0300

    return do método com parâmetros de entrada e saida

--- aula009.java
+++ aula009.java
@@ -2,12 +2,17 @@ public class aula009{
     
     public static void main(String[]args){
 
-        
+        int r;
         msg("Aprendendo Java.", 5);
-        
+        r = soma(10, 5);
+        System.out.println(r);
+    }
+    public static int soma(int n1, int n2){
+        int res = n1 + n2;
+        return res;
     }
 
-    public static void msg(String m, int l){
+    public static void msg(String m, int l ){
         for(int i = 0; i < l; i++)
         System.out.println(m);
     }
