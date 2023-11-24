8635076126512d71f6185a5d4972d2950b45aa51
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-24 06:06:13 -0300

    construindo a matriz

--- aula008.java
+++ aula008.java
@@ -5,5 +5,11 @@ public class aula008 {
         final int linhas = 3;
         final int collunas = 5;
         int[][] numeros = new int[linhas][collunas];
+
+        for(int l =0; l < linhas; l++){
+            for(int c = 0; c < collunas; c++){
+                numeros[l][c] = new SecureRandom().nextInt(100);
+            }
+        }
     }
 }
