287c9e5d8d73d0b53aacba77de370347ec47fc9e
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-24 07:07:40 -0300

    passando parâmetro para o metódo

--- aula009.java
+++ aula009.java
@@ -2,12 +2,13 @@ public class aula009{
     
     public static void main(String[]args){
 
-        msg("Jocicleudo");
-        msg("Aprendendo Java.");
-        msg("no curso CFB");
+        
+        msg("Aprendendo Java.", 5);
+        
     }
 
-    public static void msg(String m){
+    public static void msg(String m, int l){
+        for(int i = 0; i < l; i++)
         System.out.println(m);
     }
-}
+} 
