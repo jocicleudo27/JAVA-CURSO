494a9adab4828eff0f3668893b8b139aab48ec95
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-24 05:35:49 -0300

    remove

--- aula006.java
+++ aula006.java
@@ -38,7 +38,6 @@ public class aula006{
         //for sepecial para ler elementos de um array.
         final int tam = 10;
         int[] num = {9,2,7,1,8,5,3,4,0,6};
-        int[] nume = new int[tam];
         int p = 5;
         int pos;
         //System.arraycopy(num, 0, nume, 0, tam);
