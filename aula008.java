4c9fa2f08cb42c8d10ae2092ba31ef81393d4b15
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-24 06:01:56 -0300

    declarando matriz

--- aula008.java
+++ aula008.java
@@ -2,5 +2,8 @@ import java.security.SecureRandom;
 public class aula008 {
     public static void main(String[]args){
         
+        final int linhas = 3;
+        final int collunas = 5;
+        int[][] numeros = new int[linhas][collunas];
     }
 }
