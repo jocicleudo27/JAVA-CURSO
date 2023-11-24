cb59721e4c121ac27b82d9e5860329606fc2e5a5
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-24 06:56:10 -0300

    estudo dos métodos

---
+++ aula009.java
@@ -0,0 +1,13 @@
+public class aula009{
+    
+    public static void main(String[]args){
+
+        msg("Jocicleudo");
+        msg("Aprendendo Java.");
+        msg("no curso CFB");
+    }
+
+    public static void msg(String m){
+        System.out.println(m);
+    }
+}
