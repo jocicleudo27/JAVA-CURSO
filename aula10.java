4b6e2fcd2ef0b7674b353250df3295401730790d
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-25 10:05:10 -0300

    saídas de pré e pós decremento

--- aula10.java
+++ aula10.java
@@ -4,9 +4,23 @@ public class aula10{
             //teste e veja as alterações
             int num = 0;
 
-            Jogador j1 = new Jogador(num++);
-            Jogador j2 = new Jogador(num++);
-            Jogador j3 = new Jogador(num++);
+            Jogador j1 = new Jogador(++num);
+            Jogador j2 = new Jogador(++num);
+            Jogador j3 = new Jogador(++num);
+            /*
+                esta é a saida do pós decremento.
+                Jogador numero 0 criado
+
+                Jogador numero 1 criado
+
+                Jogador numero 2 criado
+
+                esta é a saida do pré decremento.
+                Jogador numero 1 criado
+                Jogador numero 2 criado
+                Jogador numero 3 criado
+
+             */
             //j1.num = 10;
             //j2.num = 12;
             //System.out.printf("%n%d",j1.num);
