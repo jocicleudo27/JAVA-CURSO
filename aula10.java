1276db034cf5b21b9fb2a48efae9d154325e7041
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-25 07:37:54 -0300

    classe e objetos criados

--- aula10.java
+++ aula10.java
@@ -4,8 +4,12 @@ public class aula10{
             Jogador j1 = new Jogador();
             Jogador j2 = new Jogador();
             Jogador j3 = new Jogador();
-        class Jogador{
-            
-        }
     }
 }
+
+    class Jogador{
+        public Jogador(){
+        System.out.printf("%nJogador criado%n ");        
+    }
+    }
+    
