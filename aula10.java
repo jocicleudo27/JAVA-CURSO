2b01297850d10f2d91877d47efcd2934129b48c2
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-25 05:55:03 -0300

    objeto criado

---
+++ aula10.java
@@ -0,0 +1,11 @@
+public class aula10{
+    public static void main(String[] args){
+        //Ex: de criação de objeto.
+            Jogador j1 = new Jogador();
+            Jogador j2 = new Jogador();
+            Jogador j3 = new Jogador();
+        class Jogador{
+            
+        }
+    }
+}
