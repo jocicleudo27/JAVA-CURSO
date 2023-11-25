65140134a1a140f1022e35f84ad354ef0acf0954
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-25 09:46:21 -0300

    diferença entre publico e privado

--- aula10.java
+++ aula10.java
@@ -4,18 +4,20 @@ public class aula10{
             Jogador j1 = new Jogador(1);
             Jogador j2 = new Jogador(2);
             
-            j1.num =10;
-            j2.num = 12;
-            System.out.printf("%n%d",j1.num);
-            System.out.printf("%n%d",j2.num);
+            //j1.num = 10;
+            //j2.num = 12;
+            //System.out.printf("%n%d",j1.num);
+            //System.out.printf("%n%d",j2.num);
     }
 }
 
     class Jogador{
-        public int num = 0;
+        
+        private int num = 0;
 
-        public Jogador(int n){
-        System.out.printf("Jogador numero %d criado%n ", n);        
-    }
+        public Jogador(int num){
+        this.num = num;
+        System.out.printf("Jogador numero %d criado%n ", num);        
+        }   
     }
     
