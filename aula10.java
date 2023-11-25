1a1b5fd85216db05196030b9319a3cef71779f77
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-11-25 11:31:20 -0300

    teste do método add vidas ok

--- aula10.java
+++ aula10.java
@@ -8,7 +8,9 @@ public class aula10{
             Jogador j2 = new Jogador(++num);
             Jogador j3 = new Jogador(++num);
             
-            j1.setVidas(100);
+            //j1.setVidas(100);
+            j1.addVidas();
+            j3.addVidas();
             System.out.printf("%nVidas do jogador 1:%d", j1.getVidas());
             System.out.printf("%nVidas do jogador 2:%d", j2.getVidas());
             System.out.printf("%nVidas do jogador 3:%d", j3.getVidas());
