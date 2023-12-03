5544465e660f63ed5986b82e704a220df4131cf3
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-12-03 13:02:52 -0300

    aula 11 class carro. estudo sobre herança

---
+++ aula11.java
@@ -0,0 +1,11 @@
+public class aula11 {
+    public static void main(String[]args){
+        class Carro{
+            private String nome;
+            private Boolean ligado;
+            private Boolean destruido;
+            private int blindagem;
+            private Boolean armamento;
+        }
+    }
+}
