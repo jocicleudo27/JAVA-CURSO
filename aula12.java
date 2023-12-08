2fae8a4b23e94c57d637b09b5f497f99d8d3a004
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-12-08 17:25:52 -0300

    programa Java para Pilha binária

---
+++ aula12.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class aula12{
+    public static void main(String [] args){
+
+    }
+
+    class MinHeap{
+        
+        //Para armazenar matriz de elementos no heap
+        private int[]heapArray;
+
+        //Tamanho máximo da pilha
+        private int capacity;
+
+        //Número atual de elementos no heap
+        private int current_heap_size;
+    }
+}
