09e2bfee1bfcdf1a1a59ecafdd9e7f960ecd8cf2
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-12-10 17:46:37 -0300

    verifica duas condições para cada vértice v:

--- aula14.java
+++ aula14.java
@@ -4,10 +4,17 @@ import java.io*;
 
 public class aula14{
     class ShortestPath{
-        static final int v = 9;
+        static final int V = 9;
         int minDistance(int dist[], Boolean SptSet[]){//Boolean sptSet é um array usado para ratrear os vertices inluidos na arvore do caminho mais curto.
             //inicializa o valor minimo
-            int min = Integer.MAX_VALUE, min_index = 1-
+            int min = Integer.MAX_VALUE, min_index = -1;
+
+            for(int v = 0 ; v < V; v++)
+//sptSet[v] == false: Isso verifica se o vértice v ainda não foi incluído na árvore do caminho mais curto.
+                if(sptSet[v] == false && dist[v] <= min){
+                    min = dist[v];//Isso atualiza o valor mínimo (min) para a distância do vértice de origem ao vértice v.
+                    min_index = v;//Isso atualiza o índice do vértice com a distância mínima para v.
+                }
         }
     }
 
