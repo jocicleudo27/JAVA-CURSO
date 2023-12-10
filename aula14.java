04848f519168419549933fcc1987a5a84031fc6d
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2023-12-10 17:26:46 -0300

    inicialização do valor minimo

--- aula14.java
+++ aula14.java
@@ -5,7 +5,10 @@ import java.io*;
 public class aula14{
     class ShortestPath{
         static final int v = 9;
-        int minDistance(int dist[], Boolean SptSet[])
+        int minDistance(int dist[], Boolean SptSet[]){//Boolean sptSet é um array usado para ratrear os vertices inluidos na arvore do caminho mais curto.
+            //inicializa o valor minimo
+            int min = Integer.MAX_VALUE, min_index = 1-
+        }
     }
 
 }
