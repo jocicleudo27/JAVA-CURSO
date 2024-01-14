9c32a51cfe41a2b0ce2aacf0e32d0571a11fc2a7
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2024-01-14 14:27:19 -0300

    class test5

--- aula51.java
+++ aula51.java
@@ -4,11 +4,20 @@ interface aula51{
     void c();
     void d();
 }
-abstract class B implements aula51{
+abstract class E implements aula51{
     public void c(){System.out.println("I am C");}
 }
-class M extends B{
+class M extends E{
     public void a(){System.out.println("I am a");}
+    public void b(){System.out.println("I am b");}
+    public void d(){System.out.println("I am d");}
 }
-public void b(){System.out.println("I am b");}
-public void d(){System.out.println("I am d");}
+class Test5{
+    public static void main(String[] args){
+        aula51 a = new M();
+        a.a();
+        a.b();
+        a.c();
+        a.d();
+    }
+}
