ed211d690b8974e155e66040abd46336ad7a2e8d
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2024-01-14 13:25:25 -0300

    abstract class e extends class

--- aula51.java
+++ aula51.java
@@ -4,3 +4,11 @@ interface aula51{
     void c();
     void d();
 }
+abstract class B implements aula51{
+    public void c(){System.out.println("I am C");}
+}
+class M extends B{
+    public void a(){System.out.println("I am a");}
+}
+public void b(){System.out.println("I am b");}
+public void d(){System.out.println("I am d");}
