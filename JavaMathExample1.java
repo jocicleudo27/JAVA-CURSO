0ca3ccfad4867e54d9cf61625ff7954277acaf11
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2024-01-15 20:52:48 -0300

    calculos em java

---
+++ JavaMathExample1.java
@@ -0,0 +1,13 @@
+public class JavaMathExample1{
+    public static void main(String[] args){
+        double x = 28;
+        double y = 4;
+
+        System.out.println("Maximum number of x and y is: "+Math.max(x, y));
+        System.out.println("Sqaure root of y is: " + Math.sqrt(y));
+        System.out.println("Power of x and y is: " + Math.pow(x, y));
+        System.out.println("Logarithm of x is: " + Math.log(x));
+        System.out.println("Logarithm of y is: " + Math.log(y));
+        
+    }
+}
