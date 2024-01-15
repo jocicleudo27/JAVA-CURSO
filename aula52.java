7f81598b0ef4935fbde9c8c05e452a5bb2282a8e
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2024-01-15 18:55:12 -0300

    class conta and get

---
+++ aula52.java
@@ -0,0 +1,11 @@
+class Conta{
+    private long acc_no;
+    private String nome;
+    private String email;
+    private float valor;
+
+    public long getAcc_no(){
+        return acc_no;
+    }
+    
+}
