2dfb65a8c88b0966323b14cb9f1f72e45c9eab2e
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2024-01-16 17:51:16 -0300

    Esta linha define uma nova classe ImmutableDemo que é public.

--- Funcionario53.java
+++ Funcionario53.java
@@ -8,3 +8,10 @@ public final class Funcionario53{
         return pancardNumber;
     }
 }
+public class ImutableDemo{
+    public static void main(String[] args){
+        Funcionario53 e = new Funcionario53("ABC123");
+        String s1 = e.getPancardNumber();
+        System.out.println("Numero Pancard: " + s1);
+    }
+}
