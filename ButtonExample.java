4a47c69316c5a64d1d40fe3c6275b2acdcbcef4b
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2024-01-18 17:17:17 -0300

    exemplo de butão

---
+++ ButtonExample.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+public class ButtonExample{
+    public static void main(String[] args){
+        //Criar instância de quadro com o rótulo 
+        Frame f = new Frame("Exemplo de botão");
+        // Criar instância de botão com rótulo 
+        Button b = new Button("clique aqui");
+        // Definir a posição do botão no quadro  
+        b.setBounds(50, 100, 80, 30);
+         // Adicionar botão ao quadro 
+         f.add(b);
+
+         // Definir tamanho, layout e visibilidade do quadro.
+
+         f.setSize(400, 400);
+         f.setLayout(null);
+         f.setVisible(true);
+         
+    }
+}
