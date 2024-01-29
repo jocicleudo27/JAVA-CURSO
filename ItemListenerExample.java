362fea2ecc1d563fc9f32c2293edc0d4063eb196
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2024-01-29 14:13:50 -0300

    corrigido

--- ItemListenerExample.java
+++ ItemListenerExample.java
@@ -1,39 +1,40 @@
-import java.awt.*;
+import javax.swing.*;
+
 import java.awt.event.*;
 
 public class ItemListenerExample implements ItemListener {
-    Checkbox checkBox1, checkBox2;
-    Label label;
+    JCheckBox checkBox1, checkBox2;
+    JLabel label;
 
     ItemListenerExample(){
-        Frame f = new Frame("Checkbox Example");
-    label = new Label();
-    label.setSize(400, 100);
-    checkBox1 = new Checkbox("C++");
-    checkBox1.setBounds(100, 100, 50, 50);
-    checkBox2 = new Checkbox("Java");
-    checkBox2.setBounds(100, 150, 50, 50);
-    f.add(checkBox1); f.add(checkBox2); f.add(label);
-    checkBox1.addItemListener(this);
-    checkBox2.addItemListener(this);
-    f.setSize(400,400);
-    f.setLayout(null);
-    f.setVisible(true);
+        JFrame f = new JFrame("Checkbox Example");
+        label = new JLabel();
+        label.setBounds(100, 200, 200, 50);
+        checkBox1 = new JCheckBox("C++");
+        checkBox1.setBounds(100, 100, 100, 50);
+        checkBox2 = new JCheckBox("Java");
+        checkBox2.setBounds(100, 150, 100, 50);
+        f.add(checkBox1); f.add(checkBox2); f.add(label);
+        checkBox1.addItemListener(this);
+        checkBox2.addItemListener(this);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
 
-    f.addWindowListener(new WindowAdapter(){
-        public void windowClosing(WindowEvent we){
-            System.exit(0);
-        }
-    });
+        f.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent we){
+                System.exit(0);
+            }
+        });
     }
     
     public void itemStateChanged(ItemEvent e){
         if(e.getSource() == checkBox1)
             label.setText("Checkbox C++: " +
             (e.getStateChange() == 1?"checked":"unchecked"));
-                if(e.getSource() == checkBox2)
-                    label.setText("Checbox Java: " +
-                    (e.getStateChange() == 1?"checked":"unchecked"));
+        if(e.getSource() == checkBox2)
+            label.setText("Checkbox Java: " +
+            (e.getStateChange() == 1?"checked":"unchecked"));
     }
     public static void main(String[] args){
         new ItemListenerExample();
