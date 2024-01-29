95b8cb546ffebd383012d3ca310cc8e1f4445df7
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2024-01-29 13:15:24 -0300

    ComboBoxExample

---
+++ ComboBoxExample.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class ComboBoxExample{
+    JFrame f;
+
+    ComboBoxExample(){
+        f = new JFrame("Exemplo de ComboBox");
+        String country[] = {"India", "Aus", "EUA","Inglaterra", "Nova Zelândia"};
+        JComboBox cb = new JComboBox(country);
+        cb.setBounds(50, 50, 90, 20);
+        f.add(cb);
+        f.setLayout(null);
+        f.setSize(400, 500);
+        f.setVisible(true);
+    }
+    public static void main(String[] args){
+        new ComboBoxExample();
+    }
+}
