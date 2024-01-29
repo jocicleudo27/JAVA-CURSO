eb3c545d5a1b3936f89e3363771020b338a5e8f1
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2024-01-29 13:55:02 -0300

    correção

--- ActionListenerExample.java
+++ ActionListenerExample.java
@@ -7,13 +7,12 @@ Se você implementar a classe ActionListener, precisará seguir 3 etapas:
 3) Substitua o método actionPerformed():
 */
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class ActionListenerExample implements ActionListener{
-    TextField tf;  // Declare tf here so it can be accessed in actionPerformed method
-
-        
+    JTextField tf;  // Declare tf here so it can be accessed in actionPerformed method
 
     public static void main(String[] args) {
         ActionListenerExample example = new ActionListenerExample();  // Create an instance of the class
@@ -21,10 +20,10 @@ public class ActionListenerExample implements ActionListener{
     }
 
     public void createUI() {
-        Frame f = new Frame("Exemplo do ActionListener");
-        tf = new TextField();
+        JFrame f = new JFrame("Exemplo do ActionListener");
+        tf = new JTextField();
         tf.setBounds(50, 50, 150, 20);
-        Button b = new Button("Clique Aqui");
+        JButton b = new JButton("Clique Aqui");
         b.setBounds(50, 100, 80, 30);
         b.addActionListener(this);  // Add the action listener to the button
         f.add(b); 
@@ -44,4 +43,5 @@ public class ActionListenerExample implements ActionListener{
         tf.setText("Bem-vindo ao Javatpoint.");
     }  
 }
+
 
