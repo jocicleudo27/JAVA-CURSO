346297a02fa6b62289c0bb3260a558258cadbad2
jocicleudo27 <134850083+jocicleudo27@users.noreply.github.com>
2024-01-30 07:30:46 -0300

    ListExample

---
+++ ListExample.java
@@ -0,0 +1,16 @@
+/*import javax.swing.*;
+
+public class ListExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        String[] data = {"Item1", "Item2", "Item3", "Item4"};
+        JList list = new JList(data);
+
+        frame.getContentPane().add(list);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
+*/
